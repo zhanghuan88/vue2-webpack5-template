85b5a5086feb885a34d7c9cdd2cc34c353d0b63b
zhanghuan <363416071@qq.com>
2022-02-28 09:08:46 +0800

    feat(first commit): first commit

--- webpack/webpack.analyzer.js
+++ webpack/webpack.analyzer.js
@@ -6,18 +6,6 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPl
 module.exports = merge(webpackProConfig, {
     plugins: [
         new BundleAnalyzerPlugin(
-            {
-                analyzerMode: 'server',
-                analyzerHost: '127.0.0.1',
-                analyzerPort: 8889,
-                reportFilename: 'report.html',
-                defaultSizes: 'parsed',
-                openAnalyzer: true,
-                generateStatsFile: false,
-                statsFilename: 'stats.json',
-                statsOptions: null,
-                logLevel: 'info'
-            }
         ),
     ]
 
