cb1f28ae115dbeb8b6e51671d82056681af666fe
zhanghuan <363416071@qq.com>
2022-03-07 11:32:21 +0800

    feat(first commit): first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import App from './App';
+import store from "./store/index";
 import './styles/index.scss';
 
 const app = new Vue({
     render: h => h(App),
+    store
 });
 app.$mount('#app');
