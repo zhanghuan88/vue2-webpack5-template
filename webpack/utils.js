ae560f4d093765bda3387d9adfc5cfc0cad8c87c
zhanghuan <363416071@qq.com>
2022-03-17 09:31:28 +0800

    feat(环境变量): 动态配置环境变量
    动态配置环境变量

--- webpack/utils.js
+++ webpack/utils.js
@@ -1,12 +1,12 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
-    resolve: function (dir) {
-        return path.join(__dirname, '..', dir)
-    },
-    readEnv: (file)=> {
-      const { parsed} = require('dotenv').config({ path: file})
-      Object.keys(parsed).forEach(key=>parsed[key] = JSON.stringify(parsed[key]))
-      return parsed;
-    }
-}
+  resolve: function(dir) {
+    return path.join(__dirname, '..', dir);
+  },
+  readEnv: (file) => {
+    const {parsed} = require('dotenv').config({path: file});
+    Object.keys(parsed).forEach(key => parsed[key] = JSON.stringify(parsed[key]));
+    return parsed;
+  }
+};
