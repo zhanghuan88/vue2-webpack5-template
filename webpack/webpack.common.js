e7f4f62ad8333e0e975cfd528c707ee926ec27c7
zhanghuan <363416071@qq.com>
2022-04-03 19:33:02 +0800

    feat(main): 修改入库口文件名称
    修改入库口文件名称

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -5,7 +5,7 @@ const {getExternals, getCdnConfig, resolve, getConditionalLoader} = require('./u
 
 module.exports = {
   entry: {
-    index: "./src/index.js"
+    index: "./src/mian.js"
   },
   output: {
     filename: "js/[name].[fullhash:8].js",
